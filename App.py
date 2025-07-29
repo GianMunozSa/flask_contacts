fcbfdb41fda9e56b44274895c8a36ba94ef31d27
GianMSa <gia.munozs@duocuc.cl>
2025-07-29 00:38:30 -0400

    inicio proyecto de contactos flask

---
+++ App.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template,request,redirect,url_for,flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+#Mysql Connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3096
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flaskcontacts'
+mysql=MySQL(app)
+
+#settings
+app.secret_key = 'mysecretkey'
+
+#Routes
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur= mysql.connection.cursor()
+        cur.execute('INSERT INTO contacts (fullname,phone,email) VALUES(%s,%s,%s)',
+                    (fullname,phone,email))
+        mysql.connection.commit()
+        flash('Contact Added Successfully')
+        return redirect(url_for('Index'))
+    
+
+@app.route('/edit')
+def edit_contact():
+    return 'edit contact'
+
+@app.route('/delete')
+def delete_contact():
+    return 'delete contact'
+
+if __name__=='__main__':
+    app.run(port=3000,debug=True)
